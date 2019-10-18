a4dc7c6e5b1a0dc4cd743696a4af4353022c92ab
Max Pumperla <max.pumperla@googlemail.com>
2019-10-18 08:16:00 +0200

    update, pypi release

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # entropy-mdlp
-Minimum description length principle algorithm in python, for optimal binning of continuous variables
+
+Minimum description length principle algorithm in python, for optimal binning of continuous variables. This
+package is a port of the respective R package of the same name. Install with
+
+```bash
+pip install entropy-mdlp
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='entropy-mdlp',
+      version='0.1',
+      description='Minimum description length principle',
+      url='http://github.com/maxpumperla/entropy-mdlp',
+      download_url='https://github.com/maxpumperla/entropy-mdlp/tarball/0.1',
+      author='Max Pumperla',
+      author_email='max.pumperla@googlemail.com',
+      install_requires=['numpy'],
+
+      packages=find_packages(),
+      license='MIT',
+      zip_safe=False,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ])
